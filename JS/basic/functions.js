aa7c507de26b70fa2d3e2ec6777bd028eb71f44c
Morning <morning@admins-iMac.local>
2022-11-30 12:36:32 +0800

    s

--- JS/basic/functions.js
+++ JS/basic/functions.js
@@ -1,7 +1,43 @@
-let chat = hi;
+// let babySharkLyrics = "18094891113 12040pgjfd04";
+// console.log(babySharkLyrics.charAt[1]);
+// console.log(babySharkLyrics.length);
+// console.log(babySharkLyrics[babySharkLyrics.length - 1]);
+// console.log(str.toUpperCase());
+// console.log(babySharkLyrics.substring(4, 5));
 
-if (chat == hi) {
-  console.log("zail");
-} else if (chat == hello) {
-  console.log("zl");
-}
+// let firstName = John;
+// let fullName = firstName.concat(` `, Smith);
+// console.log(fullName); //john smith
+// console.log(firstName); // john
+
+// let str = "this is string";
+// console.log(str.indexOf("is"));
+
+// let myString = "a this is substring is more not is";
+// console.log(myString.indexOf("is"));
+// let firstPart = myString.substring(0, myString.indexOf("is"));
+// let lastPart = myString.substring(
+//   myString.indexOf("is") + "is".length,
+//   myString.length
+// );
+// console.log(lastPart);
+// myString = firstPart.concat(lastPart);
+
+// firstPart = myString.substring(0, myString.indexOf("is"));
+// lastPart = myString.substring(
+//     myString.indexOf("is") + "is" .length
+//     myString.lengt)
+
+// let number = 123.8124982149124;
+// console.log(number.toFixed(3));
+
+// console.log(Math.ceil(-3.101));
+
+// console.log(Math.round(0.9));
+// console.log(Math.round(5.9), Math.round(5.5), Math.round(5.2));
+// console.log(Math.round(-5.9), Math.round(-5.5), Math.round(-5.2));
+// console.log(Math.max(1, 2, 3));
+// console.log(Math.pow(200, 9));
+// let a = 1,
+//   b = 6;
+// console.log(Math.floor(Math.random() * Math.abs(a - b)) + Math.min(a, b));
